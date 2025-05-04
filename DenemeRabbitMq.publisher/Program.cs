95082fda6aecff6ae27cfc59a71494ac0e1ec37c
Yusuf Ziya Demirci <yziya.demirci@istanbulticaret.edu.tr>
2025-05-04 17:11:16 +0300

    Add project files.

---
+++ DenemeRabbitMq.publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace DenemeRabbitMq.publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://hyxingls:GFLXFn53VOopBo0rDk67ZkPHfdajt2qK@toucan.lmq.cloudamqp.com/hyxingls");
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("hello-queue", durable: true, exclusive: false, autoDelete: false);
+
+            string message = "Hello World!";
+            var messagebody = System.Text.Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty, "hello-queue", null, messagebody);
+            Console.WriteLine($"Mesaj Gönderildi: {message}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DenemeRabbitMq.subscriber/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+
+namespace DenemeRabbitMq.subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://hyxingls:GFLXFn53VOopBo0rDk67ZkPHfdajt2qK@toucan.lmq.cloudamqp.com/hyxingls");
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+           // bu satırı yazmak zorunda değilsin eğer yazarsan subscriber bu kuyruğu oluşturmazsa bu otomatik oluşturur
+           // channel.QueueDeclare("hello-queue", durable: true, exclusive: false, autoDelete: false);
+
+            var consumer= new EventingBasicConsumer(channel);
+            
+            channel.BasicConsume("hello-queue", autoAck: true, consumer);
+            
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = System.Text.Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Mesaj Alındı: {message}");
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
